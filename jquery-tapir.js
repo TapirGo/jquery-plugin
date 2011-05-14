1b5a8629cdd61056eab26ed44b6f60efcc5d8de2
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-05-14 22:22:30 +0200

    Rename to Tapir

--- jquery-tapir.js
+++ jquery-tapir.js
@@ -37,13 +37,13 @@
     return results ? results[1] : false;
   }
 
-  $.fn.tapirgo = function(method) {
+  $.fn.tapir = function(method) {
     if (methods[method]) {
       return methods[ method ].apply(this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === 'object' || ! method) {
       return methods.init.apply(this, arguments);
     } else {
-      $.error('Method ' +  method + ' does not exist on jQuery.tapirgo');
+      $.error('Method ' +  method + ' does not exist on jQuery.tapir');
     }
   };
 
